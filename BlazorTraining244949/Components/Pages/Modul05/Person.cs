0230afc933b017de3dfe60418988d99efe65fb0d
Hannes Preishuber <hannesp@ppedv.de>
2025-03-14 09:57:41 +0100

    Model mit annotationen

---
+++ BlazorTraining244949/Components/Pages/Modul05/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTraining244949.Components.Pages.Modul05
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(10, MinimumLength =3, ErrorMessage = "Min 3  Maximal 10 Zeichen")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(1, 120, ErrorMessage = "Alter muss zwischen 1 und 120 liegen")]
+        public int Alter { get; set; } //doofes property
+        [Required(ErrorMessage = "Da muss was rein")]
+        public  DateTime GebDat { get; set; }
+    }
+}
